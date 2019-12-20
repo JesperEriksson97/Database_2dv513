527af205f81c8349ef9d37848d012cbd112fd0ff
Jesper Eriksson <42961415+JesperEriksson97@users.noreply.github.com>
2019-12-20 18:02:20 +0100

    Updated

--- src/sql/JSONDecoder.java
+++ src/sql/JSONDecoder.java
@@ -2,33 +2,71 @@ package sql;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.text.StringEscapeUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
 public class JSONDecoder {
-	BufferedReader reader;
-	String filePath = "Reddit2007-10.json";
 	
-	public ArrayList<JSONObject> getJSONObjetcs(String fileName) throws IOException {
+	static Connection connection = null;
+	static String databaseName = "reddit200710";
+	static String url = "jdbc:mysql://localhost:3306/" + databaseName + "?useSSL=false";
+	
+	static String username = "root";
+	static String password = "kommer10";
+	java.sql.PreparedStatement ps_id = null;
+	
+
+	BufferedReader reader;
+	String filePath = "reddit";
+	String cur;
+	BufferedReader br;
+	
+	public void createTables(String fileName) throws IOException, JSONException, SQLException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver").getDeclaredConstructor().newInstance();
+		connection = DriverManager.getConnection(url, username, password);
+
 		File file = new File(fileName);
 		String path = file.getAbsolutePath();
+		JSONObject json;
+		
 		reader = new BufferedReader(new FileReader(path));
 		String line = reader.readLine();
 		
-		ArrayList<JSONObject> jsonObjs = new ArrayList<JSONObject>();
 		
+		
+		//InputStreamReader isr = new InputStreamReader(new FileInputStream(path),"unicode");
+		//br = new BufferedReader(isr);
+		
+		//while (cur = br.readLine()) != null
+	
 		while(line != null) {
-			// and then read next line
-			jsonObjs.add(encodeToJSON(line));
+			json = encodeToJSON(line);
+			createMySqlTable(json.getString("id"), json.getString("parent_id"), json.getString("link_id"), json.getString("name"), json.getString("author"), json.getString("body"), json.getString("subreddit_id"), json.getString("subreddit"), json.getInt("score"), json.getString("created_utc"));
 			line = reader.readLine();
 		}
 		
-		return jsonObjs;
+		/** while(line != null) {
+			// and then read next line
+			jsonObjs.add(encodeToJSON(line));
+			line = reader.readLine();
+		}**/
+		
+		
 		
 	}
 
@@ -39,6 +77,29 @@ public class JSONDecoder {
 		return value;
 				 
 	}
+	
+	private void createMySqlTable(String id, String parent_id, String link_id, String name, String author, String body, String subreddit_id, String subreddit, int score, String created_utc) throws SQLException {
+		try {
+			ps_id = connection.prepareStatement("INSERT INTO `reddit200710`.`2011`(`id`, `parent_id`, `link_id`, `name`, `author`, `body`, `subreddit_id`, `subreddit`, `score`, `created_utc`) VALUES (?,?,?,?,?,?,?,?,?,?)");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		ps_id.setString(1, id);	
+		ps_id.setString(2, parent_id);	
+		ps_id.setString(3, link_id);
+		ps_id.setString(4, name);
+		ps_id.setString(5, author);
+		ps_id.setString(6, StringEscapeUtils.escapeEcmaScript(body));
+		ps_id.setString(7, subreddit_id);
+		ps_id.setString(8, subreddit);
+		ps_id.setInt(9, score);
+		ps_id.setString(10, created_utc);
+			
+		int log = ps_id.executeUpdate();
+		ps_id = null;
+		System.out.println("LOG :" + log);
+	}
+	
 	
 	
 }
--- src/sql/SQL_connection.java
+++ src/sql/SQL_connection.java
@@ -1,5 +1,8 @@
 package sql;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
@@ -7,6 +10,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.apache.commons.text.StringEscapeUtils;
 
 import org.json.JSONObject;
 
@@ -18,65 +24,41 @@ public class SQL_connection {
 	
 	static String username = "root";
 	static String password = "kommer10";
+	java.sql.PreparedStatement ps_id = null;
+	BufferedReader reader;
 	
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, SQLException, IOException {
 		
 		JSONDecoder jd = new JSONDecoder();
-		ArrayList<JSONObject> jo = jd.getJSONObjetcs("reddit");
-		System.out.println(jo.size());
-		Class.forName("com.mysql.jdbc.Driver").getDeclaredConstructor().newInstance();
-		connection = DriverManager.getConnection(url, username, password);
+		jd.createTables("RC_2011-07.txt"); // real deal
+		// jd.createTables("reddit"); // test
 		
-		for(long i = 0; i < jo.size(); i++) {
-			System.out.println("ID: " + jo.get((int) i).get("id"));
-			System.out.println("PARENT_ID: " + jo.get((int) i).get("parent_id"));
-			System.out.println("LINK_ID: " + jo.get((int) i).get("link_id"));
-			System.out.println("NAME: " + jo.get((int) i).get("name"));
-			System.out.println("AUTHOR: " + jo.get((int) i).get("author"));
-			System.out.println("BODY: " + jo.get((int) i).get("body"));
+		/* for(long i = 0; i < jo.size(); i++) {
 			
-			/**
-			 * 2019-12-04 20:30, Making progress but i need to look up on prepareStatement() now i create a new tuple for every
-			 * prepared statement execution. I want all statements to create one single row. Need to look up on that.
-			 * TODO:
-			 * 
-			 * Look up on prepareStatement();¨
-			 * Change Body to a larger VARCHAR value.
-			 */
-			PreparedStatement ps_id = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`id`, `parent_id`, `link_id`, `name`, `author`, `body`, `subreddit_id`, `subreddit`, `score`, `created_utc`) VALUES ('"+jo.get((int)i).get("id")+"', '"
+			 Creating tables.
+			ps_id = connection.prepareStatement("INSERT INTO `reddit200710`.`2011`(`id`, `parent_id`, `link_id`, `name`, `author`, `body`, `subreddit_id`, `subreddit`, `score`, `created_utc`) VALUES ('"+jo.get((int)i).get("id")+"', '"
 					+ jo.get((int) i).get("parent_id")+"', '"
 					+ jo.get((int) i).get("link_id")+"', '"
 					+ jo.get((int) i).get("name")+"', '"
 					+ jo.get((int) i).get("author")+"', '"
-					+ jo.get((int) i).get("body")+"', '"
+					+ StringEscapeUtils.escapeEcmaScript((jo.get((int) i).get("body")).toString()) +"', '"
 					+ jo.get((int) i).get("subreddit_id")+"', '"
 					+ jo.get((int) i).get("subreddit")+"', '"
 					+ jo.get((int) i).get("score")+"', '"
 					+ jo.get((int) i).get("created_utc")+"')");
 			
-			/*PreparedStatement ps_parent_id = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`parent_id`) VALUES ('" + jo.get((int) i).get("parent_id") + "')");
-			PreparedStatement ps_linked_id = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`link_id`) VALUES ('" + jo.get((int) i).get("link_id") + "')");
-			PreparedStatement ps_name = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`name`) VALUES ('" + jo.get((int) i).get("name") + "')");
-			PreparedStatement ps_author = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`author`) VALUES ('" + jo.get((int) i).get("author") + "')");
-			PreparedStatement ps_body = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`body`) VALUES ('" + jo.get((int) i).get("body") + "')");
-			PreparedStatement ps_subreddit_id = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`subreddit_id`) VALUES ('" + jo.get((int) i).get("subreddit_id") + "')");
-			PreparedStatement ps_subreddit = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`subreddit`) VALUES ('" + jo.get((int) i).get("subreddit") + "')");
-			PreparedStatement ps_score = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`score`) VALUES ('" + jo.get((int) i).get("score") + "')");
-			PreparedStatement ps_created_utc = connection.prepareStatement("INSERT INTO `reddit200710`.`2007`(`created_utc`) VALUES ('" + jo.get((int) i).get("created_utc") + "')");*/
+			// Executing Query.
+			int log = ps_id.executeUpdate();
+			System.out.println("LOG :" + log);
 			
-			ps_id.executeUpdate();
+			ps_id = null; */
+
 			
-			/*ps_parent_id.executeUpdate();
-			ps_linked_id.executeUpdate();
-			ps_name.executeUpdate();
-			ps_author.executeUpdate();
-			ps_body.executeUpdate();
-			ps_subreddit_id.executeUpdate();
-			ps_subreddit.executeUpdate();
-			ps_score.executeUpdate();
-			ps_created_utc.executeUpdate();*/
 		}
-		
+	
 	}
 	
-}
+		
+	
+	
+
